e7a8fc9b69ef2bd5eef943672d2e8e9480cafd41
Aaron Macdonald <aaronmac@clear.net.nz>
2017-10-28 00:32:49 +1300

    Vote & Winner Testing working

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Vote from './Vote'
+import Winner from './Winner'
 
 class Voting extends Component {
   render() {
--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 
 class Winner extends Component {
   render() {
-    return (  
-    <div className="winner">
-      Winner is {this.props.winner}!
-    </div>;
-    (
+    return (
+      <div className="winner">
+        Winner is {this.props.winner}!
+      </div>
+    )
   }
 }
+
+module.exports = Winner

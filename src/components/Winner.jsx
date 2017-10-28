569395f0be76919fd2c7a84ddc7691698a626e8d
Aaron Macdonald <aaronmac@clear.net.nz>
2017-10-29 11:48:39 +1300

    Using PureComponent for pure rendering od components

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-class Winner extends Component {
+class Winner extends PureComponent {
   render() {
     return (
       <div className="winner">
